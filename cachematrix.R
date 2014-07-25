d8a691ef4c13bed2a13052e831fe829a3872bc01
maen-87 <maena1987@gmail.com>
2014-07-26 02:22:15 +0300

    2nd Assignment solution
    - Add makeCacheMatrix to create special matrix object
    - Add cacheSolve to return the inverse either from cache or calculate it
    and store it in cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## this function will create a special matrix with 4 methods
+## set will set the value of the matrix and set the inverse to null
+## get will return the matrix value
+## setinverse will store the inverse of the matrix in the object
+## getinverse will read the inverse from the object
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+    inverse <- NULL
+    set <- function(value) {
+        x <<- value
+        inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) inverse <<- solve
+    getinverse <- function() inverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## A function that will test if the mean is already calculated 
+## it will read it from cache, 
+## otherwise it will be calculated and stored in the object cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    inverse <- x$getinverse()
+    if(!is.null(inverse)) {
+        message("getting cached inverse")
+        return(inverse)
+    }
+    data <- x$get()
+    inverse <- solve(data, ...)
+    x$setinverse(inverse)
+    inverse
 }
